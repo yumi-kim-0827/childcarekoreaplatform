c08a28723d2e4d673262294b38acbfea46fb3469
yumeekim <yumee0827@naver.com>
2024-08-01 14:30:40 +0900

    feat : api 주소 환경변수 처리

--- pages/api/getSeoulKdList.js
+++ pages/api/getSeoulKdList.js
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function getKdList(req, res) {
+  const KD_URI = process.env.NEXT_PUBLIC_IP_KD_URI;
+
   if (req.method === "GET") {
     try {
-      const response = await fetch(
-        "https://api.childcare.go.kr/mediate/rest/cpmsapi021/cpmsapi021/request?key=f676f59b7fcb46debcb520ebea4b48f6&arcode=26140"
-      );
+      const response = await fetch(`${KD_URI}&arcode=26140`);
       console.log(response);
       if (!response.ok) {
         throw new Error(`Error fetching data: ${response.statusText}`);
