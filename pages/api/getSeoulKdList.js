dcd78f1ede7da3c3bd0cccbd38d27c9be718e069
yumeekim <yumee0827@naver.com>
2024-08-01 15:37:03 +0900

    feat : NaverMap 주소 검색, 위도 경도 추출 추가

--- pages/api/getSeoulKdList.js
+++ pages/api/getSeoulKdList.js
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { parseStringPromise } from "xml2js";
 
 export default async function getKdList(req, res) {
   const KD_URI = process.env.NEXT_PUBLIC_IP_KD_URI;
@@ -6,15 +6,29 @@ export default async function getKdList(req, res) {
   if (req.method === "GET") {
     try {
       const response = await fetch(`${KD_URI}&arcode=26140`);
-      console.log(response);
-      if (!response.ok) {
-        throw new Error(`Error fetching data: ${response.statusText}`);
+
+      // 응답의 Content-Type 헤더를 확인합니다. XML 형식이어야 합니다.
+      const contentType = response.headers.get("content-type");
+
+      // 응답 본문을 텍스트 형태로 읽어옴
+      const text = await response.text();
+
+      // 응답의 Content-Type이 application/xml이 아니면 500 상태 코드 반환.
+      if (!contentType || !contentType.includes("application/xml")) {
+        return res.status(500).json({ error: "Invalid response format" });
       }
 
-      const data = await response.json();
+      // XML 텍스트를 JavaScript 객체로 변환
+      const data = await parseStringPromise(text);
+      // 변환된 데이터를 JSON 형식으로 클라이언트에 응답으로 보넴
       res.status(200).json(data);
     } catch (error) {
       console.error(error);
+      res.status(500).json({ error: error.message });
     }
+  } else {
+    // 만약 GET 메소드가 아닌 다른 메소드로 요청이 들어오면 405 상태 코드와 함께 오류 메시지 반환
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
--- pages/main/index.js
+++ pages/main/index.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 //components
 import Map from "@/src/components/map/Map";
+import NaverMap from "@/src/components/map/NaverMap";
 import Layout from "@/src/components/layout/Layout";
 import CardShelf from "@/src/components/layout/CardShelf";
 import Section from "@/src/components/layout/Section";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+
   const fetchGetData = async () => {
     try {
-      const response = await fetch("../api/getSeoulKdList");
-      console.log(response);
+      const response = await fetch("/api/getSeoulKdList");
+      const data = await response.json();
+      setList(data.response.item);
     } catch (error) {}
   };
 
@@ -21,12 +25,21 @@ export default function Home() {
     <>
       <Layout pageCalss="main">
         <CardShelf>
-          <Section></Section>
+          <Section>
+            <ul>
+              {list.map((item, id) => {
+                return <li key={id}>{item.craddr}</li>;
+              })}
+            </ul>
+          </Section>
         </CardShelf>
         <CardShelf>
           <Section>
             <div>
               <Map style={{ width: "100%", height: 600 }} />
+            </div>
+            <div>
+              <NaverMap />
             </div>
           </Section>
         </CardShelf>
