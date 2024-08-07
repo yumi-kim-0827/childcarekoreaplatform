fb87c2554d6b2997abebf8e9a5e19ebac55b5920
yumeekim <yumee0827@naver.com>
2024-08-07 17:16:01 +0900

    fix : next.cofig.js 배포환경 설정 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_IP_KD_URI: process.env.NEXT_PUBLIC_IP_KD_URI,
+    NEXT_PUBLIC_NCP_CLIENT_ID: process.env.NEXT_PUBLIC_NCP_CLIENT_ID,
+  },
 };
 
 module.exports = nextConfig;
