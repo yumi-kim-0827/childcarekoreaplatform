a6dc0dbc10453654783a1a45051cfae68dfff7b4
yumeekim <yumee0827@naver.com>
2024-08-12 15:26:22 +0900

    feat : 메인 홈 지도기반 어린이집 찾기 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 //components
 import Layout from "@/src/components/layout/Layout";
 import CardShelf from "@/src/components/layout/CardShelf";
 import Section from "@/src/components/layout/Section";
 import PageTitle from "@/src/components/layout/PageTitle";
 import SubjectTitle from "@/src/components/layout/SubjectTitle";
+import NaverMap from "@/src/components/map/NaverMap";
+import LocationTab from "@/src/components/tab/LocationTab";
+import TableContainer from "@/src/components/table/TableContainer";
 
 const Main = () => {
+  //지역 선택별 어린이집 리스트 배열 상태관리
+  const [list, setList] = useState([]);
+  //사용자가 선택한 어린이집 정보 상태관리
+  const [selectedKider, setSelectedKinder] = useState({});
+
+  //사용자가 시군구를 클릭하면 해당 시군구 코드를 업데이트
+  const handleSigunCodeClick = (sigunCode) => {
+    fetchKinderList(sigunCode);
+  };
+
+  const fetchKinderList = async (sigunCode) => {
+    try {
+      const response = await fetch(`/api/getTotalKdList?arcode=${sigunCode}`);
+      const data = await response.json();
+      setList(data.response.item);
+
+      // setList(data.response.item);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //사용자가 선택한 어린이집 객체 정보 업데이트
+  const handleKinderClickOnMap = (item) => {
+    setSelectedKinder(item);
+  };
+
   return (
     <Layout pageCalss="index">
       <CardShelf>
         <Section>
           <PageTitle pageName="홈" />
-          <SubjectTitle title="어린이집 목록" script="지도에서 찾아주세요" />
+          <SubjectTitle
+            title="지역별 어린이집 찾기"
+            script="지도에서 찾아주세요"
+          />
+          <LocationTab onClick={handleSigunCodeClick} />
+          <div>
+            <NaverMap list={list} onClick={handleKinderClickOnMap} />
+            <TableContainer data={selectedKider} />
+          </div>
         </Section>
         <Section>
           <SubjectTitle
