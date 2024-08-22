9cdb2a4951f7c89cde495e68dbb2159dc607cce3
yumeekim <yumee0827@naver.com>
2024-08-22 14:47:02 +0900

    fix : location tab 초기화

--- src/components/tab/LocationTab.js
+++ src/components/tab/LocationTab.js
@@ -1,48 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { TabView, TabPanel } from "primereact/tabview";
 //components
 import TextButton from "@/src/components/button/TextButton";
 //data
 import sidoList from "@/src/data/location";
 
-const LocationTab = ({ sidoName, sodiListIdx, onClick }) => {
+const LocationTab = ({ onClick }) => {
   return (
     <TabView>
-      {sidoName && sodiListIdx ? (
-        // 특정 시도의 시군구
-        <TabPanel header={sidoName}>
-          <ul className="tab-sigun-list">
-            {sidoList[sodiListIdx].sigungu.map((sigungu, id) => (
-              <li key={id}>
-                <TextButton
-                  label={sigungu.name}
-                  onClick={onClick}
-                  data={sigungu.code}
-                />
-              </li>
-            ))}
-          </ul>
-        </TabPanel>
-      ) : (
-        // 전체 시도 시군구
-        <>
-          {sidoList.map((item, id) => (
-            <TabPanel header={item.sido} key={id}>
-              <ul className="tab-sigun-list">
-                {item.sigungu.map((sigungu, id) => (
-                  <li key={id}>
-                    <TextButton
-                      label={sigungu.name}
-                      onClick={onClick}
-                      data={sigungu.code}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </TabPanel>
-          ))}
-        </>
-      )}
+      {sidoList.map((item, id) => {
+        return (
+          <TabPanel header={item.sido} key={id}>
+            <ul className="tab-sigun-list">
+              {item.sigungu.map((sigungu, id) => (
+                <li key={id}>
+                  <TextButton
+                    label={sigungu.name}
+                    onClick={onClick}
+                    data={sigungu.code}
+                  />
+                </li>
+              ))}
+            </ul>
+          </TabPanel>
+        );
+      })}
     </TabView>
   );
 };
