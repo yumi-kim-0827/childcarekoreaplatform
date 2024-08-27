cb7b1a91dc417816cb41b7af98452a60c61fcf06
yumeekim <yumee0827@naver.com>
2024-08-27 14:10:33 +0900

    remove 파일 삭제

--- src/components/layout/CardShelf.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CardShelf = ({ children }) => {
-  return <div className="card">{children}</div>;
-};
-
-export default CardShelf;
--- src/components/table/TableContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-//components
-import CustomTable from "@/src/components/table/CustomTable";
-
-const TableContainer = (data) => {
-  return <CustomTable data={data} />;
-};
-
-export default TableContainer;
